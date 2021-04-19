a0ee6ddabb63a57dd00159dcab50c014e74cd904
Amanda Carpentieri <carpentieri.a@gmail.com>
2021-04-19 15:16:07 -0700

    edit

--- src/client/components/Projects.tsx
+++ src/client/components/Projects.tsx
@@ -82,8 +82,6 @@ const Projects = (props: ProjectsProps) => {
                                <span className="techIcon m-2"> || </span>
                                <a data-tip="View Site" rel="noreferrer" target="_blank" href="https://petservices.herokuapp.com/" className='workIcon m-2'><CgWebsite /></a>
                                <span className="techIcon m-2"> || </span>
-                                <span data-tip="Screenshots" className="workIcon m-2"></span>
-                                <span className="techIcon m-2"> || </span>
                                 <span data-tip="Screenshots" className="workIcon m-2"><PetImages /></span>
                                 </div>
                                </div>
